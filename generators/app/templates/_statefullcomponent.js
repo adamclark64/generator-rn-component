68bb15c5247a38b4a682e8048cdf53a5289add2f
adamclark64 <aclark64@gmail.com>
2017-06-22 14:49:42 -0600

    eslint error fixes

--- generators/app/templates/_statefullcomponent.js
+++ generators/app/templates/_statefullcomponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
-import Styles from './style'
+import Styles from './style';
 
 
 class <%= name %> extends Component {
